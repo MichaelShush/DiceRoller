6bbcc8e4d77b48f601f632e4a5894c85afd44060
Michael Shushkovsky <Michaels1410@nycstudents.net>
2024-05-26 00:22:21 -0400

    Commit 1 very bugged

--- src/Roll.java
+++ src/Roll.java
@@ -1,22 +1,31 @@
 public class Roll {
-    private int diceRoll;
     private int diceSides;
-    private static int total;
+    private static int totalDice = 0;
+    private static int total = 0;
 
     public Roll(int sides) {
         this.diceSides = sides;
-        diceRoll = (int)(Math.random() * diceSides) + 1;
+    }
+
+    private int roll(){
+        int x = (int)(Math.random() * diceSides) + 1;
+        total += x;
+        totalDice++;
+        return x;
     }
 
     public int roll(int amount){
-        int roll = diceRoll * amount;
-        total += diceRoll;
-        return roll;
+        int t = 0;
+        for(int i = 0; i < amount; i++){
+            t += roll();
+        }
+        return t;
     }
 
     public int returnTotal(){
         int x = total;
         total = 0;
+        totalDice = 0;
         return x;
     }
 }
