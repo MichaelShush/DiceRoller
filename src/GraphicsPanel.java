7ecdb8e3a2a8cb17889bee5a280b111025af48b7
Michael Shushkovsky <Michaels1410@nycstudents.net>
2024-06-01 00:36:44 -0400

    Commit 2. Finished the buttons for singular rolls.

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,21 +1,92 @@
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import javax.swing.JButton;
-import javax.swing.JTextField;
 
-public class GraphicsPanel extends JPanel {
+public class GraphicsPanel extends JPanel implements MouseListener {
     private BufferedImage dice;
-    private double stringX;
-    private double starY;
-    private int x;
-    private JButton rollButton;
+    private final double stringX;
+    private final double starY;
+
+    //single roll variables
+    public int roll100;
+    public int roll20;
+    public int roll12;
+    public int roll10;
+    public int roll8;
+    public int roll6;
+    public int roll4;
+    public int coinFlip;
+
+    //roll button
+    private final Rectangle r = new Rectangle (50, 450, 45, 20);
+
+    //dice
+    private final Roll d100 = new Roll(100);
+    private final Roll d20 = new Roll(20);
+    private final Roll d12 = new Roll(12);
+    private final Roll d10 = new Roll(10);
+    private final Roll d8 = new Roll(8);
+    private final Roll d6 = new Roll(6);
+    private final Roll d4 = new Roll(4);
+    private final Roll coin = new Roll(2);
+
+    //rectangles
+    private final Rectangle rect100 = new Rectangle (25, 100, 45, 20);
+    private final Rectangle rect20 = new Rectangle (25, 145, 45, 20);
+    private final Rectangle rect12 = new Rectangle (25, 190, 45, 20);
+    private final Rectangle rect10 = new Rectangle (25, 235, 45, 20);
+    private final Rectangle rect8 = new Rectangle (25, 280, 45, 20);
+    private final Rectangle rect6 = new Rectangle (25, 325, 45, 20);
+    private final Rectangle rect4 = new Rectangle (25, 370, 45, 20);
+    private final Rectangle coinRect = new Rectangle (25, 415, 45, 20);
+
+    //button pressed?
+    public boolean d100Pressed;
+    public boolean d20Pressed;
+    public boolean d12Pressed;
+    public boolean d10Pressed;
+    public boolean d8Pressed;
+    public boolean d6Pressed;
+    public boolean d4Pressed;
+    public boolean coinPressed;
+
+    //only one button is on the rest are false(off)
+    public void oneOn(int trueNum){
+        d100Pressed = false;
+        d20Pressed = false;
+        d12Pressed = false;
+        d10Pressed = false;
+        d8Pressed = false;
+        d6Pressed = false;
+        d4Pressed = false;
+        coinPressed = false;
+        if(trueNum == 100){
+            d100Pressed = true;
+        }else if(trueNum == 20){
+            d20Pressed = true;
+        }else if(trueNum == 12){
+            d12Pressed = true;
+        }else if(trueNum == 10){
+            d10Pressed = true;
+        }else if(trueNum == 8){
+            d8Pressed = true;
+        }else if(trueNum == 6){
+            d6Pressed = true;
+        }else if(trueNum == 4){
+            d4Pressed = true;
+        }else if(trueNum == 2){
+            coinPressed = true;
+        }
+    }
+
 
     public GraphicsPanel() {
+        addMouseListener(this);
         stringX = 0;
         starY = 50;
         try {
@@ -23,40 +94,41 @@ public class GraphicsPanel extends JPanel {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        rollButton = new JButton("roll");
-        Roll d100 = new Roll(100);
-        Roll d20 = new Roll(20);
-        Roll d12 = new Roll(12);
-        Roll d10 = new Roll(10);
-        Roll d8 = new Roll(8);
-        Roll d6 = new Roll(6);
-        Roll d4 = new Roll(4);
-        Roll coin = new Roll(2);
-        d6.roll(2);
-        d4.roll(5);
-        d100.roll(1);
-        x = d20.returnTotal();
-    }
 
-    public void actionPerformed(ActionEvent e){
-        if(e.getSource() instanceof JButton){
-            JButton button = (JButton) e.getSource();
-            if(button == rollButton){
+        JFrame f = new JFrame();
+        f.addMouseListener(this);
+
+        MouseListener mouseListener = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
 
             }
-        }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
     }
+
 
     @Override
     public void paintComponent(Graphics g) {
-//        Roll d100 = new Roll(100);
-//        Roll d20 = new Roll(20);
-//        Roll d12 = new Roll(12);
-//        Roll d10 = new Roll(10);
-//        Roll d8 = new Roll(8);
-//        Roll d6 = new Roll(6);
-//        Roll d4 = new Roll(4);
-//        Roll coin = new Roll(2);
         super.paintComponent(g);  // just do this
         g.setColor(Color.BLUE);
         g.drawRect(25, 100, 45, 20);
@@ -65,7 +137,6 @@ public class GraphicsPanel extends JPanel {
         g.drawRect(25, 235, 45, 20);
         g.drawRect(25, 280, 45, 20);
         g.drawRect(25, 325, 45, 20);
-        g.drawRect(25, 370, 45, 20);
         g.drawRect(25, 370, 45, 20);
         g.drawRect(25, 415, 45, 20);
         g.drawImage(dice, 200, (int) starY, null);
@@ -81,7 +152,87 @@ public class GraphicsPanel extends JPanel {
         g.drawString("D6", 26, 340);
         g.drawString("D4", 26, 385);
         g.drawString("coin", 26, 430);
-        g.drawString("Result: " + x, 100, 115);
-        rollButton.setLocation(50, 450);
+        g.drawString("Result: " + d100.returnTotal(), 100, 115);
+        g.drawRect(50, 450, 45, 20);
+        if(d100Pressed){
+            g.drawString("" + roll100, 70, 115);
+            Roll.clearTotal();
+        }else if(d20Pressed){
+            g.drawString("" + roll20, 70, 160);
+            Roll.clearTotal();
+        }else if(d12Pressed){
+            g.drawString("" + roll12, 70, 205);
+            Roll.clearTotal();
+        }else if(d10Pressed){
+            g.drawString("" + roll10, 70, 250);
+            Roll.clearTotal();
+        }else if(d8Pressed){
+            g.drawString("" + roll8, 70, 295);
+            Roll.clearTotal();
+        }else if(d6Pressed){
+            g.drawString("" + roll6, 70, 340);
+            Roll.clearTotal();
+        }else if(d4Pressed){
+            g.drawString("" + roll4, 70, 385);
+            Roll.clearTotal();
+        }else if(coinPressed){
+            g.drawString("" + coinFlip, 70, 430);
+            Roll.clearTotal();
+        }
+        g.drawString("" + d100.getTotalDice(), 70, 450);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(e.getButton() == MouseEvent.BUTTON1){
+            Point mouseClickLocation = e.getPoint();
+            if(r.contains(mouseClickLocation)){
+                roll100 = d100.roll();
+            }else if(rect100.contains(mouseClickLocation)){
+                roll100 = d100.roll();
+                oneOn(100);
+            }else if(rect20.contains(mouseClickLocation)){
+                roll20 = d20.roll();
+                oneOn(20);
+            }else if(rect12.contains(mouseClickLocation)){
+                roll12 = d12.roll();
+                oneOn(12);
+            }else if(rect10.contains(mouseClickLocation)){
+                roll10 = d10.roll();
+                oneOn(10);
+            }else if(rect8.contains(mouseClickLocation)){
+                roll8 = d8.roll();
+                oneOn(8);
+            }else if(rect6.contains(mouseClickLocation)){
+                roll6 = d6.roll();
+                oneOn(6);
+            }else if(rect4.contains(mouseClickLocation)){
+                roll4 = d4.roll();
+                oneOn(4);
+            }else if(coinRect.contains(mouseClickLocation)){
+                coinFlip = coin.roll();
+                oneOn(2);
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- src/Roll.java
+++ src/Roll.java
@@ -1,43 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Roll {
-    private int diceSides;
-    private int diceNum;
-    private int total;
+    private final int diceSides;
+    private static int total;
     private static int totalDice = 0;
-    private static int allTotal = 0;
+    private ArrayList<Integer> rollNums = new ArrayList<>();
 
     public Roll(int sides) {
         this.diceSides = sides;
-        diceNum = 0;
-
     }
 
-    private int roll(){
+    public int roll(){
         int x = (int)(Math.random() * diceSides) + 1;
         total += x;
         totalDice++;
         return x;
     }
 
+    //Rolls a certain amount of dice and saves each dice roll to the classes rollNums.
     public int roll(int amount){
         int t = 0;
+        int x;
         for(int i = 0; i < amount; i++){
-            t += roll();
+            x = roll();
+            t += x;
+            rollNums.add(x);
         }
         return t;
     }
-//ok so what you want to do is to add something to roll all dice and also a total for each individual iteration of the Roll class.
+
     public int returnTotal(){
         int x = total;
         total = 0;
         totalDice = 0;
+        rollNums.clear();
         return x;
     }
 
-    public void rollAllDice(ArrayList<Roll> all){
-        for(Roll i : all){
-            roll(diceNum);
-        }
+    public static void clearTotal(){
+        total = 0;
+//        totalDice = 0;
+    }
+
+    public int getTotalDice(){
+        return totalDice;
     }
 }
